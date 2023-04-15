36d65e20cbad22ff8d73f7bb8626dca0e3b79f13
coolCicada <1475395755@qq.com>
2023-04-15 15:44:24 +0800

    review: 1521/C

--- 个人构造练习/1000-1200/9.1512.C.cpp
+++ 个人构造练习/1000-1200/9.1512.C.cpp
@@ -6,66 +6,59 @@ typedef long long LL;
 void solve() {
   int a, b;
   cin >> a >> b;
-  string s;
-  cin >> s;
+  string str;
+  cin >> str;
 
-  for (int i = 0; i < s.size(); i ++) if (s[i] == '0') a --;
+  int length = str.size();
+  for (int i = 0; i < length; i ++) {
+    if (str[i] == '0') a --;
+  }
 
-  int l = 0, r = s.size() - 1;
-  int t1 = 0, t2 = 0;
-  while (l <= r) {
-    if (s[l] != '?' && s[r] != '?' && s[l] != s[r]) {
+  int l = 0, r = length - 1;
+  while (l < r) {
+    if (str[l] == '0' && str[r] == '1' || str[r] == '0' && str[l] == '1') {
       cout << -1 << endl;
       return;
     }
-    if (l == r && s[l] == '?' && s[r] == '?') t1 ++;
-    else if (s[l] == '?' && s[r] == '?') t2 ++;
-    else if (s[l] == '?' && s[r] == '0' || s[l] == '0' && s[r] == '?') {
+    if (str[l] == '0' && str[r] == '?' || str[r] == '0' && str[l] == '?') {
       a --;
-      s[l] = s[r] = '0';
+      str[l] = str[r] = '0';
     }
     l ++, r --;
-  }
-
-  if (a < 0) {
-    cout << -1 << endl;
-    return;
-  }
-
-  if (a % 2 == 1 && t1 == 0) {
-    cout << -1 << endl;
-    return;
-  }
-
-  if (a % 2 == 0 && t2 * 2 < a) {
-    cout << -1 << endl;
-    return;
   }
 
   if (a % 2 == 1) {
-    s[(s.size() - 1) / 2] = '0';
+    if (length % 2 == 0) {
+      cout << -1 << endl;
+      return;
+    }
+    if (str[length >> 1] != '?') {
+      cout << -1 << endl;
+      return;
+    }
+    str[length >> 1] = '0';
     a --;
   }
 
-  l = 0, r = s.size() - 1;
+  l = 0, r = length - 1;
   while (l < r && a > 0) {
-    if (s[l] == '?' && s[r] == '?') {
+    if (str[l] == '?' && str[r] == '?') {
       a -= 2;
-      s[l] = s[r] = '0';
+      str[l] = str[r] = '0';
     }
     l ++, r --;
   }
 
-  if (a > 0) {
+  if (a != 0) {
     cout << -1 << endl;
     return;
   }
 
-  for (int i = 0; i < s.size(); i ++) {
-    if (s[i] == '?') s[i] = '1';
+  for (int i = 0; i < length; i ++) {
+    if (str[i] == '?') str[i] = '1';
   }
 
-  cout << s << endl;
+  cout << str << endl;
 }
 
 int main() {
