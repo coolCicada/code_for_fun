3799a9aa9a2cf1f038a269db8f410ed570985c1e
coolCicada <1475395755@qq.com>
2023-04-18 22:05:40 +0800

    feat: AC B. Prinzessin der Verurteilung

--- 个人构造练习/1000-1200/B_Prinzessin_der_Verurteilung.cpp
+++ 个人构造练习/1000-1200/B_Prinzessin_der_Verurteilung.cpp
@@ -10,28 +10,24 @@ void solve() {
   cin >> s;
 
   unordered_set<string> st;
-  for (int i = 0; i < n; i ++) st.insert(string(1, s[i]));
+  string tmp = "";
+  for (int i = 0; i < n; i ++) st.insert(tmp + s[i]);
   for (int i = 0; i < n - 1; i ++) st.insert(s.substr(i, 2));
   for (int i = 0; i < n - 2; i ++) st.insert(s.substr(i, 3));
 
-  vector<string> v = { "" };
-  for (int i = 0; i < 26; i ++) {
-    int L = v.size();
-    cout << L << endl;
-    for (int j = 0; j < L; j ++) {
-      // cout << "!" << endl;
-      v.push_back(v[j] + char('a' + i));
-    }
-  }
-
-  cout << "??" << endl;
+  vector<string> v = {""};
 
   for (int i = 0; i < v.size(); i ++) {
-    if (!st.count(v[i])) {
-      cout << v[i] << endl;
-      return;
+    for (int j = 0; j < 26; j ++) {
+      string curr = v[i] + char('a' + j);
+      if (!st.count(curr)) {
+        cout << curr << endl;
+        return;
+      }
+      v.push_back(curr);
     }
   }
+
 }
 
 int main() {
--- 个人构造练习/1000-1200/README.md
+++ 个人构造练习/1000-1200/README.md
@@ -1,3 +1,4 @@
 - 20230415 [A. Common Prefixes](https://codeforces.com/problemset/problem/1384/A)
 - 20230416 [C. Mocha and Hiking](https://codeforces.com/problemset/problem/1559/C)
 - 20230417 [B. BerSU Ball](https://codeforces.com/problemset/problem/489/B)
+- 20230418 [B. Prinzessin der Verurteilung](https://codeforces.com/contest/1536/problem/B)
