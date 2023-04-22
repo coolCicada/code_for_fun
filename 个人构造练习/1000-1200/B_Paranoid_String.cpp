8aae9af256c74a31ae204d564a52850d1267c69e
coolCicada <1475395755@qq.com>
2023-04-22 19:17:44 +0800

    review: B. Paranoid String

--- 个人构造练习/1000-1200/B_Paranoid_String.cpp
+++ 个人构造练习/1000-1200/B_Paranoid_String.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 typedef long long LL;
 
-void solve() {
-  int n;
-  cin >> n;
-  string s;
-  cin >> s;
+vector<string> stringSplit(const std::string& str, string delim) { int previous = 0, current = -1, L = delim.size(); vector<string> elems; while ((current = str.find(delim, previous)) != string::npos) { if (current > previous) { elems.push_back(str.substr(previous, current - previous)); } previous = current + L; } if (previous != str.size()) { elems.push_back(str.substr(previous)); } return elems; }
 
+void solve() {
+  int n; cin >> n;
+  string s; cin >> s;
   LL cnt = n;
   for (int i = 1; i < n; i ++) {
-    if (s[i - 1] != s[i]) cnt += i; 
+    if (s[i] != s[i - 1]) {
+      cnt += i;
+    }
   }
-
   cout << cnt << endl;
 }
 
@@ -21,8 +21,7 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  int t;
-  cin >> t;
+  int t; cin >> t;
   while (t --) solve();
 
   return 0;
