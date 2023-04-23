8251e5a6a9648db63fa29ed3cd4d43d392a09cdc
coolCicada <1475395755@qq.com>
2023-04-23 21:27:19 +0800

    review: C. Dora and Search

--- 个人构造练习/1000-1200/C_Dora_and_Search.cpp
+++ 个人构造练习/1000-1200/C_Dora_and_Search.cpp
@@ -6,53 +6,29 @@ typedef long long LL;
 vector<string> stringSplit(const std::string& str, string delim) { int previous = 0, current = -1, L = delim.size(); vector<string> elems; while ((current = str.find(delim, previous)) != string::npos) { if (current > previous) { elems.push_back(str.substr(previous, current - previous)); } previous = current + L; } if (previous != str.size()) { elems.push_back(str.substr(previous)); } return elems; }
 
 void solve() {
-  int m; cin >> m;
-  vector<int> a(m + 1);
-  priority_queue<int, vector<int>, greater<int> > pq1;
-  priority_queue<int, vector<int>, less<int> > pq2;
-  for (int i = 1; i <= m; i ++) {
-    cin >> a[i];
-    pq1.push(a[i]);
-    pq2.push(a[i]);
-  }
-  int l = 1, r = m;
+  int n; cin >> n;
+  vector<int> a(n + 1); for (int i = 1; i <= n; i ++) cin >> a[i];
+  int l = 1, r = n;
+  int mn = 1, mx = n;
   while (l < r) {
-    int minV = pq1.top();
-    int maxV = pq2.top();
-    bool flag = true;
-    if (a[l] == minV) {
-      l ++;
-      pq1.pop();
-      flag = false;
+    if (a[l] == mn) mn ++, l ++;
+    if (a[l] == mx) mx --, l ++;
+    if (a[r] == mn) mn ++, r --;
+    if (a[r] == mx) mx --, r --;
+
+    if (a[l] != mn && a[l] != mx && a[r] != mn && a[r] != mx) {
+      cout << l << ' ' << r << endl;
+      return;
     }
-    if (a[l] == maxV) {
-      l ++;
-      pq2.pop();
-      flag = false;
-    }
-    if (a[r] == minV) {
-      r --;
-      pq1.pop();
-      flag = false;
-    }
-    if (a[r] == maxV) {
-      r --;
-      pq2.pop();
-      flag = false;
-    }
-    if (flag) break;
   }
-  if (l < r) cout << l << ' ' << r;
-  else cout << -1;
-  cout << endl;
+  cout << -1 << endl;
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  int t;
-  cin >> t;
+  int t; cin >> t;
   while (t --) solve();
 
   return 0;
