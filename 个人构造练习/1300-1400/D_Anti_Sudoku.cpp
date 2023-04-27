6899e8141a95df0daa4194bef8c360cd44a687dd
coolCicada <1475395755@qq.com>
2023-04-27 22:40:08 +0800

    review: D. Anti-Sudoku

--- 个人构造练习/1300-1400/D_Anti_Sudoku.cpp
+++ 个人构造练习/1300-1400/D_Anti_Sudoku.cpp
@@ -5,22 +5,25 @@ typedef long long LL;
 
 vector<string> stringSplit(const std::string& str, string delim) { int previous = 0, current = -1, L = delim.size(); vector<string> elems; while ((current = str.find(delim, previous)) != string::npos) { if (current > previous) { elems.push_back(str.substr(previous, current - previous)); } previous = current + L; } if (previous != str.size()) { elems.push_back(str.substr(previous)); } return elems; }
 
+void solve() {
+  vector<string> a(9);
+  for (int i = 0; i < 9; i ++) cin >> a[i];
+  for (int i = 0; i < 9; i ++) {
+    for (int j = 0; j < 9; j ++) {
+      if (a[i][j] == '1') a[i][j] = '2';
+    }
+  }
+  for (int i = 0; i < 9; i ++) {
+    cout << a[i] << endl;
+  }
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
   int t; cin >> t;
-  while (t --) {
-    vector<string> a(9);
-    for (int i = 0; i < 9; i ++) cin >> a[i];
-    for (int i = 0; i < 9; i ++) {
-      for (int j = 0; j < 9; j ++) {
-        if (a[i][j] == '1') cout << '2';
-        else cout << a[i][j];
-      }
-      cout << endl;
-    }
-  }
+  while (t --) solve();
 
   return 0;
 }
